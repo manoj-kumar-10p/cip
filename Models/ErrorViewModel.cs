a114e7cd34ab336c4d8f09355175665ad5587852
Manoj Kumar <manoj.kumar@10pearls.com>
2021-10-11 12:13:07 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cip.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
